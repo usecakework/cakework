75b409b24a730fe1d91c00643b51d7868238eefe
Jessie Young <jessie@sahale.io>
2022-11-17 00:03:49 -0800

    Added client to speech analytics example

--- examples/examples/speech_analytics/index.js
+++ examples/examples/speech_analytics/index.js
@@ -10,7 +10,7 @@
     Audio transcription is a long-running and failure-prone process.
 */
 
-import { Sahale, sleep, succeed, fail } from 'sahale';
+import { Sahale, sleep, succeed, fail, WorkflowClient } from 'sahale';
 import startTranscription from 'startTranscription';
 import getTranscription from 'getTranscription';
 import getSegments from 'getSegments';
@@ -82,3 +82,21 @@ app.registerWorkflow(runSpeechAnalytics);
 
 // Start the application in the Sahale cloud and start listening for triggers.
 app.start();
+
+// Start a workflow execution
+// Connect to the Sahale server location
+const connection = await Connection.connect();
+
+const client = new WorkflowClient({connection});
+
+const handle = await client.start(runSpeechAnalytics, {
+    args: {
+        'audioFileUri': 'https://my_bucket_prefix/my_audio_file.mp4',
+        'numSegments': 5
+    },
+    taskQueue: 'speech-analytics'
+});
+console.log(`Started workflow ${handle.workflowId}`);
+
+// optional: wait for client result
+console.log(await handle.result()); // SUCCESS, myAnalyticsJSONUri
