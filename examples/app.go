061838d20b8aecf8141bed5040ea13ebeb721bad
Jessie Young <jessie@sahale.io>
2022-12-05 16:42:14 -0800

    dummy commit to test fly deployment

--- examples/app.go
+++ examples/app.go
@@ -20,9 +20,9 @@ type album struct {
 
 // albums slice to seed record album data.
 var albums = []album{
-    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+    {ID: "3", Title: "Reputation", Artist: "Taylor Swift", Price: 56.99},
+    {ID: "4", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "5", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 func main() {
