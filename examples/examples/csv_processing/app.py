69122d46967fbcb2ff34ee502adf3f2242ab11dd
Jessie Young <jessie@sahale.io>
2022-12-10 23:53:45 -0800

    convert tabs to spaces

--- examples/examples/csv_processing/app.py
+++ examples/examples/csv_processing/app.py
@@ -7,33 +7,33 @@ import requests
 import json
 
 def csv_processor(url):
-	count = 0
-	with requests.Session() as s:
-		download = s.get(url)
-		decoded_content = download.content.decode('utf-8')
-		cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-		my_list = list(cr)
-		for row in my_list:
-			count += int(row[0])
-	return count
+    count = 0
+    with requests.Session() as s:
+        download = s.get(url)
+        decoded_content = download.content.decode('utf-8')
+        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+        my_list = list(cr)
+        for row in my_list:
+            count += int(row[0])
+    return count
 
 
 async def main():
-	# create and deploy the activity
-	app = App()
-	app.register_activity(csv_processor, "CsvProcessor")
-	app.start()
+    # create and deploy the activity
+    app = App()
+    app.register_activity(csv_processor, "CsvProcessor")
+    app.start()
 
-	# invoking the activity
-	client = Client()
-	id = await client.start_new("CsvProcessor", request)
-	logging.info(f"Started activity with ID = '{id}'.")
+    # invoking the activity
+    client = Client()
+    id = await client.start_new("CsvProcessor", request)
+    logging.info(f"Started activity with ID = '{id}'.")
 
-	# check the status of the activity invocation
-	time.sleep(3)
-	response = client.get_status(id)
-	if response.status == 'SUCCEEDED':
-		logging.info(f"Activity succeeded with result: = '{json.dumps(response.output)}'.")
+    # check the status of the activity invocation
+    time.sleep(3)
+    response = client.get_status(id)
+    if response.status == 'SUCCEEDED':
+        logging.info(f"Activity succeeded with result: = '{json.dumps(response.output)}'.")
 
 if __name__ == "__main__":
-	main()
+    main()
