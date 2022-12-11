46d94ff604da9d31d1e3d5460db27c0f8f9cb2ca
Jessie Young <84425236+jessie-young@users.noreply.github.com>
2022-12-11 15:23:50 -0800

    pass app name and user id as parameters to Client

--- examples/examples/csv_processing/start_activity.py
+++ examples/examples/csv_processing/start_activity.py
@@ -3,7 +3,7 @@ import json
 
 async def main():
     url = "https://my-bucket.s3.amazonaws.com/file.csv"
-    client = Client()
+    client = Client("my-app", "my-user-id") # TODO remove user id parameter and infer this from the logged-in credentials isntead 
     id = client.start_new_activity("csv-processor", url)
     result = client.get_activity(id)
     if result.status == 'SUCCEEDED':
