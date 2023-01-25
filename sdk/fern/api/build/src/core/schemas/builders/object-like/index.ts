641e555eea4a0a281bff78d355c3a56f8d856c27
Jessie Young <jessie@sahale.io>
2023-01-25 15:26:42 -0800

    fern
    Former-commit-id: be59f2976b3d0cd3cc60e8b86a5679729bde94df

---
+++ sdk/fern/api/build/src/core/schemas/builders/object-like/index.ts
@@ -0,0 +1,2 @@
+export { getObjectLikeUtils, withProperties } from "./getObjectLikeUtils";
+export { OBJECT_LIKE_BRAND, type BaseObjectLikeSchema, type ObjectLikeSchema, type ObjectLikeUtils } from "./types";
---
+++ sdk/fern/api/build/src/resources/imdb/client/index.ts
@@ -0,0 +1,2 @@
+export * as createMovie from "./createMovie";
+export * as getMovie from "./getMovie";
