7266f9e37ec5f1445373d86ee9bb4e20f9326cfc
Eric Chen <eric@sahale.io>
2023-01-30 18:56:39 -0800

    replace useeffect with just a funciton

--- sdk/javascript/react/useRun.js
+++ sdk/javascript/react/useRun.js
@@ -1,36 +1,31 @@
-import { useEffect, useState, useRef } from "react"
+import { useEffect, useState, useRef, useCallback } from "react"
 import CakeworkClient from "@cakework/client"
 
-function useRun(token, project, task, parameters, compute) {
-    const [status, setStatus] = useState("INITIALIZED");
+function useRun() {    
+    const [runId, setRunId] = useState("");
+    const [status, setStatus] = useState("");
     const [result, setResult] = useState(null);
-    
-    const didRun = useRef(false);
 
-    useEffect(() => {
-        if (didRun.current === true) {
-            return
-        }
-        didRun.current = true;
+    async function run(token, project, task, parameters, compute) {
+        setRunId("");
+        setRunId("");
+        setResult(null);
 
-        async function run() {
-            const cakework = new CakeworkClient(project, token); 
-            const runResponse = await cakework.run(task, parameters, compute);
-            var status;
-            do {
-                await new Promise(r => setTimeout(r, 500));
-                status = await cakework.getRunStatus(runResponse.runId);
-                setStatus(status);
-            } while (status !== "SUCCEEDED" && status !== "FAILED");
+        const cakework = new CakeworkClient(project, token);
+        const runResponse = await cakework.run(task, parameters, compute);
+        setRunId(runResponse.runId);
+        var status;
+        do {
+            await new Promise(r => setTimeout(r, 1000));
+            status = await cakework.getRunStatus(runResponse.runId);
+            setStatus(status);
+        } while (status !== "SUCCEEDED" && status !== "FAILED");
 
-            const result = await cakework.getRunResult(runResponse.runId);
-            setResult(result);
-        }   
+        const result = await cakework.getRunResult(runResponse.runId);
+        setResult(result);
+    }       
 
-        run();
-    }, [])    
-
-    return [status, result];
+    return [runId, status, result, run];
 }
 
 export default useRun;
