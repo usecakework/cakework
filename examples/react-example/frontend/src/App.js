14c53e98cccf2212582da8219bf0c5ea1cfad1cf
Eric Chen <eric@cakework.com>
2023-01-30 22:28:39 -0800

    Update App.js

--- examples/react-example/frontend/src/App.js
+++ examples/react-example/frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div>This is an example of using a react hook to call Cakework.</div>
-      <button onClick={() => run("264d23638f85a2b344595b91e04735d6c63a59bd9aaf9ae9505585b98c1af030", "react-example-backend", "say-hello", {"name":"jessie"})}>Start me!</button>
+      <button onClick={() => run("YOUR_CLIENT_TOKEN_HERE", "react-example-backend", "say-hello", {"name":"jessie"})}>Start me!</button>
       <div>Your Run ID: {runId}</div>
       <div>Run Status: {status}</div>
       <div>Run Result: {result}</div>
